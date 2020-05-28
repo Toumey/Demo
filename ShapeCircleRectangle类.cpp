d2c8a295053d5f8a2caf10a2401469be41afc48b
Toumey <60951914+Toumey@users.noreply.github.com>
2020-05-28 12:17:07 +0800

    Add files via upload

---
+++ ShapeCircleRectangle类.cpp
@@ -0,0 +1,64 @@
+#include <iostream>
+using namespace std;
+const float pi=3.14159;
+ class Shape{
+ 	public:
+ 		Shape(int a);
+		int getID();
+ 		int getArea();
+		~Shape();
+	private:
+		int m_ID;  
+ };
+ class Circle:public Shape{
+ 	private:
+ 		int r;
+ 	public:
+ 		Circle(int b,int a):r(b),Shape(a){
+		 	cout<<"Circle constructor called:"<<getID()<<endl;
+		 }
+ 		double getArea(){
+ 			return r*r*pi;
+		 }
+ 		~Circle(){
+ 			cout<<"Circle destructor called:"<<getID()<<endl;
+		 }
+ };
+ class Rectangle:public Shape{
+ 	private:
+ 		int h,w;
+ 	public:
+ 		Rectangle(int a,int b,int c):h(a),w(b),Shape(c){
+ 			cout<<"Rectangle constructor called:"<<getID()<<endl;
+		 }
+ 		int getArea(){
+ 			return h*w;
+		 }
+ 		~Rectangle(){
+ 			cout<<"Rectangle destructor called:"<<getID()<<endl;
+		 }
+ };
+ Shape::Shape(int a){
+ 	m_ID=a;
+ 	cout<<"Shape constructor called:"<<m_ID<<endl;
+ }
+ int Shape::getID(){
+ 	return m_ID;
+ }
+ int Shape::getArea(){
+ 	return 0;
+ }
+ Shape::~Shape(){
+ 	cout<<"Shape destructor called:"<<m_ID<<endl;
+ }
+
+int main()
+{
+    Shape s(1);//1表示ID
+    Circle c(4,2);//4表示半径，2表示ID
+    Rectangle r(4, 5,3);//4和5表示长和宽，3表示ID
+    cout<<"Shape的面积"<<s.getArea()<<endl;
+    cout<<"Circle的面积"<<c.getArea()<<endl;
+    cout<<"Rectangle的面积"<<r.getArea()<<endl;
+}
+
---
+++ motorcycle类.cpp
@@ -0,0 +1,61 @@
+#include "iostream"
+using namespace std;
+
+class vehicle{
+	private:
+		int maxspeed,weight;
+	public:
+		vehicle(int a,int b):maxspeed(a),weight(b){
+			cout<<"vehicle constructor called. "<<"maxspeed:"<<maxspeed<<"; weight"<<weight<<endl;
+		}
+		void run(){
+			cout<<"vehicle run"<<endl;
+		}
+		void stop(){
+			cout<<"vehicle stop"<<endl;
+		}
+		~vehicle(){
+			cout<<"vehicle destructor called. "<<"maxspeed:"<<maxspeed<<"; weight"<<weight<<endl;
+		}
+};
+class bicycle:virtual public vehicle{
+	private:
+		int height;
+	public:
+		bicycle(int a,int b,int c):vehicle(a,b),height(c){
+			cout<<"bicycle constructor called. height:"<<height<<endl;
+		}
+		~bicycle(){
+			cout<<"bicycle destructor called. height:"<<height<<endl;
+		}
+};
+class motorcar:virtual public vehicle{
+	private:
+		int seatnum;
+	public:
+		motorcar(int a,int b,int c):vehicle(a,b),seatnum(c){
+			cout<<"motorcar constructor called. seatnum:"<<seatnum<<endl;
+		}
+		~motorcar(){
+			cout<<"motorcar destructor called. seatnum:"<<seatnum<<endl;
+		}
+};
+class motorcycle:public bicycle,public motorcar{
+	public:
+		motorcycle(int a,int b,int c,int d):bicycle(c,d,a),motorcar(c,d,b),vehicle(c,d){
+			cout<<"motorcycle constructor called"<<endl;
+		}
+		~motorcycle(){
+			cout<<"motorcycle destructor called"<<endl;
+		}
+};
+
+
+int main(int argc, char *argv[])
+{
+    motorcycle m(1, 2, 3, 4);//1表示height，2表示setnum，3表示maxspeed，4表示weight
+    m.run();
+    m.stop();
+    return 0;
+}
+
