ecd33a51a079a9c5b9a1446eef41924c1fbceb51
Toumey <60951914+Toumey@users.noreply.github.com>
2020-06-01 19:28:41 +0800

    Add files via upload

---
+++ 运算符重载成员函数（复数类）.cpp
@@ -0,0 +1,73 @@
+#include <iostream>
+using namespace std;
+
+class Complex{
+	public:
+		Complex(double a=0,double b=0)
+		{
+			real=a;
+			imag=b;
+		}
+		Complex operator + (Complex c2)
+		{
+			Complex c;
+			c.real=c2.real+real;
+			c.imag=c2.imag+imag;
+			return Complex(c.real,c.imag);
+		}
+		Complex operator + (double x)
+		{
+			Complex c;
+			c.real=real+x;
+                        c.imag=imag;
+			return Complex(c.real,c.imag); 
+		}
+		Complex operator - (Complex c2)
+		{
+			Complex c;
+			c.real=real-c2.real;
+			c.imag=imag-c2.imag;
+			return Complex(c.real,c.imag);
+		}
+		Complex operator - (double x)
+		{
+			Complex c;
+			c.real=real-x;
+                        c.imag=imag;
+			return Complex(c.real,c.imag);
+		}
+		void print()
+		{
+			cout<<real<<"+"<<imag<<"i"<<endl;
+		}
+	private:
+		double real;
+		double imag;
+}; 
+
+
+
+int main()
+{
+    Complex a(3,4),b(1,-1),c;
+    c=a+b;
+    cout<<"a+b=";
+    c.print();
+    cout<<endl;
+    c=a-b;
+    cout<<"a-b=";
+    c.print();
+    cout<<endl;
+    c=a+100;
+    cout<<"a+100=";
+    c.print();
+    cout<<endl;
+    c=a-2.5;
+    cout<<"a-2.5=";
+    c.print();
+    cout<<endl;
+
+    return 0;
+}
+
+
